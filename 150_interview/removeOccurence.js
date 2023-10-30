9a6e9265a5bf2675e7f6b3771ee3b6b0783cf980
Oneharry <harryofficialchidi@yahoo.com>
2023-10-30 23:32:30 +0100

    leetcoding

--- 150_interview/removeOccurence.js
+++ 150_interview/removeOccurence.js
@@ -4,20 +4,17 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
+    
     let count = 0;
-    nums
-    for(let i = 0; i < nums.length; i++) {
-        let el = nums[i];
-        if(el === val) {
-            nums.splice(i, 1);
-        } else {
+    nums.forEach(el => {
+        if(el !== val) {
+            nums[count] = el
             count++;
-        }
-    }
-    console.log(count);
+        } 
+    })
     return count;
 };
 
-const nums = [0,1,2,2,3,0,4,2], val = 2;
-
+const nums = [3,2,2,3]
+const val = 3;
 console.log(removeElement(nums, val));
