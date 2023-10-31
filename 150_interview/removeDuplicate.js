738a6cd8f26b944c5557b10a93bce68436e5f7a4
Oneharry <harryofficialchidi@yahoo.com>
2023-10-31 12:46:21 +0100

    implemented remove dublicates

--- 150_interview/removeDuplicate.js
+++ 150_interview/removeDuplicate.js
@@ -8,11 +8,12 @@ var removeDuplicates = function(nums) {
     nums.forEach(num => {
         if(!arr.includes(num)) {
             arr.push(num);
-            nums[0] = num;
+            nums[count] = num;
             count++;
         }
     })
+    console.log(nums);
     return count;
 };
 
-console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
